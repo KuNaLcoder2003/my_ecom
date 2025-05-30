6f650c7ec8c72bbbc03c418378e5507737de082c
Kunal Singh <kunalindia59@gmail.com>
2025-05-31 00:49:14 +0530

    optimizing cart functionality , handling edge cases in adding or deleting a product from cart

--- backend/src/routes/cart.ts
+++ backend/src/routes/cart.ts
@@ -4,61 +4,65 @@ import { User, Product, Cart } from "../db";
 
 
 import mongoose from "mongoose";
+import { isUser } from "../middlewares/isUser";
 const cartRouter = express.Router();
 
-interface Product_desc{
-    product_id : mongoose.Types.ObjectId ,
-    product_quantity : number ,
-   
+interface Product_desc {
+    product_id: mongoose.Types.ObjectId,
+    product_quantity: number,
+
 }
 
 interface Product_arr {
-    product_name : string , product_description : string , product_price : number , product_stock : number , _id : mongoose.Schema.Types.ObjectId
+    product_name: string, product_description: string, product_price: number, product_stock: number, _id: mongoose.Schema.Types.ObjectId
 }
 
-cartRouter.post('/' , async(req : Request , res : Response)=>{
-    const product_to_add : Product_desc = req.body.product;
-    const userId = req.body.userid
+cartRouter.post('/', isUser, async (req: any, res: Response) => {
+    const product_to_add: Product_desc = req.body.product;
+    const userId = req.id
+    console.log(product_to_add)
 
     try {
-        const product = await Product.findOne({_id : new mongoose.Types.ObjectId(product_to_add.product_id)})
-        if(!product) {
+        const product = await Product.findOne({ _id: new mongoose.Types.ObjectId(product_to_add.product_id) })
+        if (!product) {
             res.status(403).json({
-                message : 'The Product no longer exists',
-                valid : false,
+                message: 'The Product no longer exists',
+                valid: false,
             })
             return
         }
 
-        if(product.product_stock < product_to_add.product_quantity){
+        if (product.product_stock < product_to_add.product_quantity) {
             res.status(403).json({
-                valid : false,
-                message : 'Less stock',
-                product : {
-                    id : product_to_add.product_id,
-                    quantity : product_to_add.product_quantity - product.product_stock
+                valid: false,
+                message: 'Less stock',
+                product: {
+                    id: product_to_add.product_id,
+                    quantity: product_to_add.product_quantity - product.product_stock
                 }
             })
-            return            
+            return
         }
 
         // check if cart already exixts
         const cart = await Cart.findOne({
-            user_id : userId , checked_out : false
+            user_id: userId, checked_out: false
         })
-        if(!cart) {
+        if (!cart) {
+            console.log('new cart')
             // create new cart
             let amount = product.product_price * product_to_add.product_quantity
             const new_cart = await Cart.create({
-                user_id : userId , 
-                products : [{product_to_add}],
-                amount : amount
+                user_id: userId,
+                products: [product_to_add],
+                amount: amount,
+                checked_out: false
             })
 
             res.status(200).json({
-                valid : true,
-                message : 'Added to cart Successfully',
-                cart : new_cart
+                valid: true,
+                message: 'Added to cart Successfully',
+                cart: new_cart
             })
             return
         }
@@ -68,30 +72,127 @@ cartRouter.post('/' , async(req : Request , res : Response)=>{
 
             let amount = product.product_price * product_to_add.product_quantity
 
-            const updated = await cart.updateOne({$push : {products : product_to_add} , $inc : {amount : amount}})
+            // const updated = await cart.updateOne({$push : {products : product_to_add} , $inc : {amount : amount}})
+            let updated;
 
-            if(!updated) {
+            // first check if product already exists
+
+            const existingProductIndex = cart.products.findIndex(
+                (product) => product.product_id == product_to_add.product_id
+            );
+
+            if (existingProductIndex >= 0) {
+                console.log('exists')
+                // Product exists: update its quantity
+                const existingProduct = cart.products[existingProductIndex];
+
+
+                const updatedCart = cart.products.map((product) => {
+                    if (product.product_id == product_to_add.product_id) {
+                        return {
+                            product_id: product.product_id,
+                            product_quantity : product_to_add.product_quantity > product.product_quantity ? product_to_add.product_quantity + product.product_quantity : product_to_add.product_quantity  
+                        }
+                    } else {
+                        return product
+                    }
+                })
+                console.log(updatedCart)
+
+                
+
+                updated = await cart.updateOne({ products: updatedCart, $inc: { amount: amount } })
+
+
+            } else {
+                // Product doesn't exist: add it to the cart
+                updated = await cart.updateOne(
+                    { $push: { products: product_to_add }, $inc: { amount: amount } }
+                );
+            }
+
+            if (!updated) {
                 res.status(402).json({
-                    valid : false,
-                    message : 'Cannot update cart'
+                    valid: false,
+                    message: 'Cannot update cart'
                 })
                 return
-            } 
+            }
 
             res.status(200).json({
-                valid : true,
-                message : 'Cart updated'
+                valid: true,
+                message: 'Cart updated'
             })
         }
 
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            message : "Error : " + error ,
-            valid : false,
+            message: "Error : " + error,
+            valid: false,
         })
     }
 
+})
+
+cartRouter.get('/products', isUser, async (req: any, res: Response) => {
+    const userId = req.id;
+    try {
+        const cart = await Cart.findOne({ user_id: userId }).populate('products.product_id')
+        if (!cart) {
+            res.status(404).json({
+                message: 'No Active Cart'
+            })
+            return
+        }
+        res.status(200).json({
+            valid: true,
+            cart: cart
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Error : " + error,
+            valid: false,
+        })
+    }
+})
+
+cartRouter.delete('/item' , isUser , async(req : any , res : Response)=> {
+    const userId = req.id;
+    const product_id= req.body.product;
+    console.log(product_id);
+    try {
+        const cart = await Cart.findOne({user_id : userId})
+        if(!cart) {
+            res.status(40).json({
+                message : 'Bad request',
+                valid : false
+            })
+            return
+        }
+        const updated_products = cart.products.filter(product => product.product_id != product_id);
+        console.log(updated_products)
+        const updated = await cart.updateOne({products : updated_products}).populate('products.product_id')
+        if(!updated) {
+            res.status(402).json({
+                message : 'Unable to delete at the moment',
+                valid : false
+            })
+            return;
+        }
+        res.status(200).json({
+            message : 'Product deleted',
+            valid : true,
+            cart : updated
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Error : " + error,
+            valid: false,
+        })
+    }
 })
 
 
--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -9,7 +9,7 @@ const upload = multer({ storage: storage })
 
 
 interface Product {
-    product_name : string , product_description : string , product_price : number , product_stock : number 
+    product_name: string, product_description: string, product_price: number, product_stock: number , category : string
 }
 
 const productRouter = express.Router();
@@ -17,8 +17,8 @@ const productRouter = express.Router();
 productRouter.post('/', isAdmin, upload.array('images'), async (req: Request, res: Response) => {
     console.log(req.body)
     try {
-        const {product_name , product_description , product_price , product_stock } = req.body as Product;
-        if(!product_description || !product_name || !product_price || !product_stock) {
+        const { product_name, product_description, product_price, product_stock , category } = req.body as Product;
+        if (!product_description || !product_name || !product_price || !product_stock) {
             res.status(400).json({
                 valid: false,
                 message: 'Important fields empty'
@@ -42,29 +42,30 @@ productRouter.post('/', isAdmin, upload.array('images'), async (req: Request, re
             buffer_array.push(Buffer.from(bytes))
         })
 
-        
+
 
         const result = await uploadImages(buffer_array)
-      
+
 
         const urls = result.map(item => {
-            return {url : item.url}
+            return { url: item.url }
         })
 
         const new_product = await Product.create({
-            product_name : product_name , 
-            product_description : product_description,
-            product_price : product_price , 
-            product_stock : product_stock,
-            product_images : urls
+            product_name: product_name,
+            product_description: product_description,
+            product_price: product_price,
+            product_stock: product_stock,
+            product_images: urls,
+            category : category
         })
 
         res.status(200).json({
-            valid : true,
-            message : 'Product added successfully',
+            valid: true,
+            message: 'Product added successfully',
             new_product
         })
-        
+
 
     } catch (error) {
         console.log(error);
@@ -76,31 +77,126 @@ productRouter.post('/', isAdmin, upload.array('images'), async (req: Request, re
     }
 })
 
-productRouter.get('/' , async(req  : Request, res : Response ) => {
+productRouter.get('/search/:searchValue', async (req: Request, res: Response) => {
+    const searchValue = req.params.searchValue
+    console.log(searchValue);
     try {
-        const products = await Product.find({})
-        if(products.length == 0) {
-            res.status(402).json({
-                valid : false,
-                message : 'Cannot fetch products at the moment'
+        if (!searchValue) {
+            res.status(400).json({
+                valid: false,
+                message: 'No value to search'
             })
             return
         }
-        const products_arr  = products.map((item)=>{
+        const products = await Product.find({
+            $text: {
+                $search: searchValue,
+                $caseSensitive: false
+            }
+        })
+
+        if (products.length == 0) {
+            res.status(404).json({
+                message: 'No products found',
+                valid: false
+            })
+            return
+        }
+
+        const arr = products.map((prouct) => {
             return {
-                id : item._id,
-                name : item.product_name,
-                price : item.product_price,
-                stock : item.product_stock,
+                id: prouct._id,
+                name: prouct.product_name
+            }
+        })
+
+        res.status(200).json({
+            valid: true,
+            products: arr
+        })
+
+
+
+
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            valid: false,
+            message: 'Something went wrong',
+            err: error
+        })
+
+    }
+})
+
+productRouter.get('/', async (req: Request, res: Response) => {
+    try {
+        const products = await Product.find({})
+        if (products.length == 0) {
+            res.status(402).json({
+                valid: false,
+                message: 'Cannot fetch products at the moment'
+            })
+            return
+        }
+        const products_arr = products.map((item) => {
+            return {
+                id: item._id,
+                name: item.product_name,
+                price: item.product_price,
+                stock: item.product_stock,
+                image : item.product_images[0]
             }
         })
         res.status(200).json({
-            valid : true,
-            products :  products_arr
+            valid: true,
+            products: products_arr
         })
+        console.log(products_arr)
     } catch (error) {
         res.status(500).json({
-            message : 'Something went wrong'
+            message: 'Something went wrong'
+        })
+    }
+})
+
+productRouter.get('/item/:id' , async(req :Request , res : Response)=> {
+    const id = req.params.id
+    
+    try {
+        if(!id || id.length < 8) {
+            res.status(400).json({
+                message : 'Bad request , can not fetch product',
+                valid : false
+            })
+            return
+        }  
+
+        const product = await Product.findOne({_id : id})
+
+        if(!product) {
+            res.status(404).json({
+                message : 'Product not found',
+                valid : false,
+            })
+            return
+        }
+
+        res.status(200).json({
+            valid : true,
+            product : {
+                id : product._id,
+                name : product.product_name,
+                desc : product.product_description,
+                images : product.product_images,
+                price : product.product_price
+            }
+        })
+    } catch (error) {
+         res.status(500).json({
+            message: 'Something went wrong'
         })
     }
 })
